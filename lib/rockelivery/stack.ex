9c6dbda7651ddaea5f1d10d9dfc78d31606ce978
KauanCarvalho <kauan_calasans@hotmail.com>
2022-05-21 14:25:16 -0300

    Uses GenServer to basic stack

---
+++ lib/rockelivery/stack.ex
@@ -0,0 +1,46 @@
+defmodule Rockelivery.Stack do
+  @moduledoc false
+
+  use GenServer
+
+  def start_link(initial_stack) when is_list(initial_stack) do
+    GenServer.start(__MODULE__, initial_stack)
+  end
+
+  def push(pid, element) do
+    GenServer.cast(pid, {:push, element})
+  end
+
+  def pop(pid) do
+    GenServer.call(pid, :pop)
+  end
+
+  @impl true
+  def init(stack), do: {:ok, stack}
+
+  @impl true
+  # SYNC
+  def handle_call({:push, element}, _from, stack) do
+    new_stack = [element | stack]
+
+    {:reply, new_stack, new_stack}
+  end
+
+  @impl true
+  # SYNC
+  def handle_call(:pop, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  @impl true
+  # SYNC
+  def handle_call(:pop, _from, []) do
+    {:reply, nil, []}
+  end
+
+  @impl true
+  # ASYNC
+  def handle_cast({:push, element}, stack) do
+    {:noreply, [element | stack]}
+  end
+end
